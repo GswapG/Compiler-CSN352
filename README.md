ecd393df1eff2e594668d8beb5d7df4576c380cc
GswapG <swappyontube@gmail.com>
2025-02-03 13:20:15 +0530

    gitignore and testcases

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Compiler-CSN352
+This is a compiler for a C like language made as a course project for CSN352: Compiler Design course at IIT Roorkee.
+
+## How to run 
+### Lexer
+1.  Put your testcases in 'testcases' folder as '.c' files.
+2.  Run the following command in the project root-
+```bash
+python lexer.py
+```
--- testcases/test1.c
+++ testcases/test1.c
@@ -1,3 +1,18 @@
+#include<stdio.h>
+
 int main(){
-    printf("Hello World");
+    int l = 0;
+    int r = 90000;
+    int mid;
+    while(l<=r){
+        mid = l + (r-l)/2;
+        if(mid*mid<=90000){
+            l = mid+1;
+        }
+        else{
+            r = mid-1;
+        }
+    }
+    printf("%d",r);
+    return 0;
 }
