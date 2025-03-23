bd92cbff6b9159cc990f804646b0d0311ef43b4a
kh4rg0sh <mmukulkhedekar34@gmail.com>
2025-03-23 21:55:33 +0530

    some more fixes

--- tests/testing/scopes.c
+++ tests/testing/scopes.c
@@ -1,17 +1,4 @@
-struct A { 
-    int x;
-    float f;
-    char c;
-};
-
-struct B {
-    int y;
-    float g;
-    char d;
-};
-
 int main() {
-    struct A a = {1, 4.4};
-    struct B b = {a.x, a.f};
-
+    int x = 2; 
+    int *p = x;
 }
