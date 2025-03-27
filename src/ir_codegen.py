1a231be82526590f2a039a474d89a8ce93d3e3fa
GswapG <swappyontube@gmail.com>
2025-03-27 21:34:43 +0530

    Added basic utils for IR Generation

--- src/ir_codegen.py
+++ src/ir_codegen.py
@@ -1,5 +1,28 @@
 from tree import *
 from symtab_new import *
 
-def parseTree_to_3AC(root , symtab):
-  pass
+class IRGenerator:
+    def __init__(self):
+        self.temp_counter = 0
+        self.label_counter = 0
+        self.function_labels = {}
+
+    def new_temp(self):
+        """Generate a new unique temporary variable."""
+        temp_var = f't{self.temp_counter}'
+        self.temp_counter += 1
+        return temp_var
+
+    def new_label(self, func_name=None):
+        """
+        Generate a new unique label. 
+        If a function name is provided, use it as the label. (No overloading is supported)
+        """
+        if func_name:
+            if func_name not in self.function_labels:
+                self.function_labels[func_name] = func_name
+            return self.function_labels[func_name]
+        
+        label = f'L{self.label_counter}'
+        self.label_counter += 1
+        return label
